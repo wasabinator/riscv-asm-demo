cd72fa045cef48f69c5f0a707a570d5b37f006a2
wasabinator <6946957+wasabinator@users.noreply.github.com>
2026-05-09 13:31:18 +1000

    Blit fill working as macro

--- blitter.s
+++ blitter.s
@@ -1,3 +1,9 @@
+# BLiT (BLock Image Transfer) operations. Named this blitter as a sign of affection to the Amiga
+#
+# Uses macros heavily to avoid repeated blocks when I add all the various operations.
+# These obviously expand in the assembled code for zero runtime overhead.
+#
+
 # calculate offsets, which is used for moving along pixels
 # the display is rotated, so the origin is at the bottom left
 # of the physical display that the user sees. that means the
@@ -27,76 +33,36 @@
     sub     \out_base, \out_base, t0    # subtract y * 4 bytes
 .endm
 
+# the body of the blitter loops.
+# uses t5 & t6 as counters
+.macro blit_loop w, h, stash, innerop, advance
+    mv      t6, \h
+1337:
+    mv      t5, \w     # rotated so we loop w * h
+    \stash             # stash state needed for later advance
+1338:
+    \innerop           # inner blit operation goes here
+    addi    t5, t5, -1
+    bnez    t5, 1338b
+
+    \advance           # advance based on previously stashed state
+    addi    t6, t6, -1
+    bnez    t6, 1337b
+.endm
+
+.macro op_fill fbuffer, colour, xoffset
+    sw      a7, 0(t1)
+    add     t1, t1, t2
+.endm
+
 # fill(dst_buffer=a0, dst_width=a1, dst_height=a2, dst_x=a3, dst_y=a4, dst_w=a5, dst_h=a6, colour=a7)
 .global fill
 fill:
     # t1 = frame buffer origin
     # t2 = x position increment value
-    # we don't need to calc y incrementer since we just move +4 bytes because this is a portrait display rotated
+    # we don't need to calc y incrementer since we just move -4 bytes because this is a portrait display rotated
     calc_origin a0, a1, a2, a3, a4, t1, t2
 
-    mv      t4, a6         # number of rows to write (screen is rotated so this is src_w)
-
-.rows:
-    mv      t3, a5         # number of pixels to write (src_h because rotated)
-    mv      t6, t1         # copy pos
-
-.row:
-    sw      a7, 0(t1)
-
-    # advance to next column
-    add     t1, t1, t2
-
-    # check if col done
-    addi    t3, t3, -1
-    bgtz    t3, .row
-
-    # advance to next row
-    addi    t1, t6, -4
-
-    # check if rows done
-    addi    t4, t4, -1
-    bgtz    t4, .rows
-
+    # run the blit loop, we only need to copy t1 as we are only writing to one frame buffer
+    blit_loop a5, a6, "mv t3, t1", op_fill, "addi t1, t3, -4"
     ret
-
-#blit s1, s10, s5, op_copy
-#blit s1, s10, s5, "li t3, 0x00FF0000"
-
-# define operations as macros
-.macro op_copy
-    # t3 already has src value, nothing to do
-.endm
-
-.macro op_fill colour
-    li      t3, \colour
-.endm
-
-.macro op_and src2
-    lw      t4, 0(t1)
-    and     t3, t3, t4
-.endm
-
-# the body of the blitter loops. uses t6 & t7 as counters
-.macro blit_loop w, h, stash, innerop, advance
-    mv      t7, \h
-1337:
-    mv      t6, \w     # rotated so we loop w * h
-    \stash             # stash state needed for later advance
-1338:
-    \innerop           # inner blit operation goes here
-    addi    t6, t6, -1
-    bnez    t6, 1338b
-
-    \advance           # advance based on previously stashed state
-    addi    t7, t7, -1
-    bnez    t7, 1337b
-.endm
-
-.macro stash src, tmp
-    mv      \tmp, \src
-.endm
-
-.macro advance src, tmp, offset
-    add     \src, \tmp, \offset
-.endm
